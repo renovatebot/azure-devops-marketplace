357354c445c3bb4b7a005e2e7ae853cdaad5cb8a
Jesse Houwing <jesse.houwing@gmail.com>
2023-01-05 08:51:37 +0100

    Update sariftools/scans

--- 01-extension-security-scan.ps1
+++ 01-extension-security-scan.ps1
@@ -97,10 +97,10 @@ foreach ($extension in $extensions)
     & git config --local user.email "jesse.houwing@gmail.com"
     & git config --local user.name "Jesse Houwing"
     
-    & git status --exit-code
+    & git add .
+    & git diff HEAD --exit-code
     if ($LASTEXITCODE -ne 0)
     {
-        & git add .
         & git commit -m "Update $publisherId/$extensionId"
         & git push
     }
