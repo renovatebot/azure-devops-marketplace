efc58ce9672286b8c5775f6d075aac2e0e456872
Jesse Houwing <jesse.houwing@gmail.com>
2023-02-23 22:15:57 +0100

    Use tfx 0.14

--- 01-update-extension-cache.ps1
+++ 01-update-extension-cache.ps1
@@ -93,7 +93,7 @@ write-output "::group::Installing tfx"
 
 if (-not (get-command -all "tfx" -ErrorAction SilentlyContinue))
 {
-    & npm install tfx-cli@^0.12 -g --no-fund
+    & npm install tfx-cli@^0.14 -g --no-fund
 }
 $token = $env:AZURE_DEVOPS_PAT
 $marketplace = "https://marketplace.visualstudio.com"
