bdd6174fee19f011739cd010ac6d67e62520b5bb
Jesse Houwing <jesse.houwing@gmail.com>
2026-01-03 00:57:32 +0100

    Update 01-update-extension-cache.ps1

--- 01-update-extension-cache.ps1
+++ 01-update-extension-cache.ps1
@@ -117,7 +117,7 @@ function write-commit {
 
 write-output "::group::Installing tfx"
 
-npm install tfx-cli@0.21.3 -g --no-fund
+npm install tfx-cli@0.22.6 -g --no-fund
 
 $token = $env:AZURE_DEVOPS_PAT
 $marketplace = "https://marketplace.visualstudio.com"
@@ -249,5 +249,6 @@ write-commit -message "Update .cache"
 if (-not $skipCommit) {
     & git push
 }
+
 
 
